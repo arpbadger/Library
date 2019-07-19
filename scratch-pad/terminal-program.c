e38f12a68e9ce972ec4e41f13664c07c31e60ff6
arpbadger <arpbadger@protonmail.com>
2019-07-19 08:22:52 -0400

    update

---
+++ scratch-pad/terminal-program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void usage();
+
+int main() {
+    char command;
+
+    // usage();
+    printf("Enter a Command\n\n");
+    scanf("%s", &command);
+    printf("%s", &command);
+    
+    return 0;
+}
+
+void usage(){
+    printf("Welcome to the program\n\n");
+    printf("Usage:\n--s : run script\n--e : exit program\n--f : create file ");
+}
