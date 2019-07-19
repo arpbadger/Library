8ec9812a3bb26cba01dd470f43813d9da8b9b94a
arpbadger <arpbadger@protonmail.com>
2019-07-19 11:25:26 -0400

    Update terminal-program.c

--- scratch-pad/terminal-program.c
+++ scratch-pad/terminal-program.c
@@ -17,3 +17,12 @@ void usage(){
     printf("Welcome to the program\n\n");
     printf("Usage:\n--s : run script\n--e : exit program\n--f : create file ");
 }
+
+void test_func(){
+    int blank_variable;
+    char list_variable[] = {1, 2, 3};
+    
+    prinft("This is a test func\n\n");
+    printf("Im using a new keyboard to test this fucntion\n\n");
+
+}
